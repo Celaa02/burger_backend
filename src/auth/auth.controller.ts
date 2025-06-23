f718b2554f993e26d79415a0d1d76e836fd887cf
Darlys Vergara <darlys0794@gmail.com>
2025-06-23 16:25:16 -0500

    Fix: remove Swagger deploy and create build

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,9 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
-import { ApiTags } from '@nestjs/swagger';
 
-@ApiTags('Auth')
 @Controller('auth')
 @Controller('auth')
 export class AuthController {
